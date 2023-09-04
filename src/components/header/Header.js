eb40fd813f72ab31e41236057318532ec2a3879d
Agutr0t <abak.93@gmail.com>
2023-09-04 15:00:20 +0200

    feat: implement header with menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
-import { AppBar, IconButton, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Button,
+  Fade,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import aurilLogo from "../../logos/Auril Logo.png";
 import { ArrowBack } from "@mui/icons-material";
 import MenuIcon from "@mui/icons-material/Menu";
+import { useLocation, useNavigate } from "react-router";
+import classNames from "classnames";
 
 function Header() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const navigateTo = useNavigate();
+  const location = useLocation();
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = (direction) => {
+    setAnchorEl(null);
+    switch (direction) {
+      case "Homepage":
+        navigateTo(`/`);
+        break;
+      case "Games":
+        navigateTo(`/Games`);
+        break;
+      case "Engines":
+        navigateTo(`/Engines`);
+        break;
+      case "About":
+        navigateTo(`/About`);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
-    <AppBar position="fixed" color="primary" sx={{ bgcolor: "primary.light" }}>
+    <AppBar
+      position="fixed"
+      color="primary"
+      elevation={0}
+      sx={{ bgcolor: "primary.light"}}
+    >
       <img src={aurilLogo} alt="auril Logo" className="Banner_Logo"></img>
-      <Toolbar sx={{ justifyContent: "space-between" }}>
+      <Toolbar sx={{ justifyContent: "space-between"}}>
         <IconButton size="large" edge="start" color="inherit" aria-label="menu">
           <ArrowBack />
         </IconButton>
-        <IconButton size="large" edge="end" color="inherit" aria-label="menu">
+        <IconButton
+          size="large"
+          edge="end"
+          color="inherit"
+          aria-label="menu"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleMenu}
+        >
           <MenuIcon />
         </IconButton>
+        <Menu
+          className="Menu"
+          id="menu-appbar"
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+          marginThreshold={0}
+          TransitionComponent={Fade}
+          anchorReference="anchorPosition"
+          anchorPosition={{ top: 56, left: 0 }}
+        >
+          <MenuItem>
+            <Typography variant="button">MENU</Typography>
+          </MenuItem>
+          <MenuItem>
+            <Typography variant="button">SESSIONS</Typography>
+          </MenuItem>
+          <MenuItem>
+            <Typography variant="button">DIES</Typography>
+          </MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
--- src/theme.js
+++ src/theme.js
@@ -3,15 +3,15 @@ import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#1976d2',
-      light: '#42a5f5',
-      dark: '#1565c0',
-      contrastText: '#fff',
+      main: "#1976d2",
+      light: "#42a5f5",
+      dark: "#1565c0",
+      contrastText: "#fff",
     },
     secondary: {
-      main: '#FFF7E1',
-      dark: '#FFE6A0',
-      contrastText: '#929292',
+      main: "#FFF7E1",
+      dark: "#FFE6A0",
+      contrastText: "#929292",
     },
   },
   components: {
@@ -22,58 +22,36 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: '"Nunito Sans", "Arial", sans-serif',
+    fontFamily: '"Bree Serif", "Arial", sans-serif',
     fontSize: 16,
     h1: {
-      fontSize: "4rem", //40px
-      fontWeight: 500, //Semi-bold
-      letterSpacing: "10px",
+      fontSize: "5rem", //48px
+      fontWeight: 400, //Regular
       color: "#fff",
-      marginLeft: "10px",
-      textTransform: "uppercase",
     },
     h2: {
       fontSize: "4rem", //40px
-      fontWeight: 500, //Semi-bold
-      letterSpacing: "10px",
+      fontWeight: 400, //Regular
       color: "#fff",
-      marginLeft: "10px",
-      textTransform: "uppercase",
     },
     h3: {
-      fontSize: "2rem", //24px
-      fontWeight: 500, //Semi-bold
-      letterSpacing: "6px",
-      color: "#F5AC10",
-      marginLeft: "6px",
-      textTransform: "uppercase",
+      fontSize: "3rem", //32px
+      fontWeight: 400, //Regular
+      color: "#fff",
     },
     h4: {
       fontSize: "1.5rem", //20px
       fontWeight: 400, //Regular
-      letterSpacing: "6px",
-      color: "#F5AC10",
-      marginLeft: "6px",
-    },
-    h5: {
-      fontSize: "1rem", //16px
-      fontWeight: 300, //Light
-      letterSpacing: "4px",
-      color: "#fff",
-      marginLeft: "4px",
-    },
-    h6: {
-      fontSize: "1rem", //16px
-      fontWeight: 200, //Extra-Light
-      color: "#fff",
+      color: "#929292",
     },
     body1: {
       fontSize: "1rem", //16px
-      fontWeight: 300, //Light
+      fontWeight: 400, //Regular
+      color: "#252525",
     },
-    caption: {
-      fontSize: "1rem", //16px
-      fontWeight: 200, //Extra-Light
+    button: {
+      fontSize: "2rem", //24px
+      fontWeight: 400, //Regular
       color: "#fff",
     },
   },
